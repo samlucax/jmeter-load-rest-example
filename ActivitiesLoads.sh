880e326af3f10ff72604f3cb4dc5b4e3f506ff0b
Samuel Lucas <samuelborgeslucas@gmail.com>
2019-09-17 22:33:13 -0300

    second commit xD

---
+++ ActivitiesLoads.sh
@@ -0,0 +1,12 @@
+source JMeterLoadTemplate.sh
+
+#############################################################################
+################################### TESTS ###################################
+#############################################################################
+
+##### ARGS: testPlanName logFileName numOfThreads rampUpPeriod loopCount ######
+
+run_jmeter_test "JMeterLoadTemplateActivities" "activities_3_parallel" 1 1 1;
+# run_jmeter_test "JMeterLoadTemplateActivities" "activities-5-parallel" 5 1 1;
+# run_jmeter_test "JMeterLoadTemplateActivities" "activities-10-parallel" 5 1 3;
+# and so on ...
---
+++ JMeterLoadTemplate.sh
@@ -0,0 +1,31 @@
+###############################################################################
+############################### JMETER TEMPLATE ###############################
+###############################################################################
+
+jmeterHomeDir=apache-jmeter-5.1.1
+
+#mock or app
+environment="app"
+mockHostAddress=
+
+if test $environment = "app"
+then
+    myHost="fakerestapi.azurewebsites.net/api/";
+elif test $environment = "mock"
+then
+    myHost=$mockHostAddress;
+fi
+
+run_jmeter_test(){
+    local testPlanNameLocal=$1;
+    local logFileNameLocal=$2;
+    local numOfThreadsLocal=$3;
+    local rampUpPeriodLocal=$4;
+    local loopCountLocal=$5;
+
+    echo "Teste iniciado:";
+    echo "Executando:     $jmeterHomeDir/bin/jmeter -n -t testPlans/"${testPlanNameLocal}".jmx -L jmeter=INFO -JnumOfThreads=$numOfThreadsLocal -JrampUpPeriod=$rampUpPeriodLocal -JloopCount=$loopCountLocal -Jhost=$myHost -l testLogs/"${logFileNameLocal}".jtl \n";
+    $jmeterHomeDir/bin/jmeter -n -t testPlans/"${testPlanNameLocal}".jmx -L jmeter=INFO -JnumOfThreads=$numOfThreadsLocal -JrampUpPeriod=$rampUpPeriodLocal -JloopCount=$loopCountLocal -Jhost=$myHost -l testLogs/"${logFileNameLocal}".jtl ;
+    echo "Teste finalizado. Gerando relatório HTML: \n $jmeterHomeDir/bin/jmeter -d $jmeterHomeDir -g testLogs/"${logFileNameLocal}".jtl -o testDashboards/"${logFileNameLocal}" -f \n"
+    $jmeterHomeDir/bin/jmeter -d $jmeterHomeDir -g testLogs/"${logFileNameLocal}".jtl -o testDashboards/"${logFileNameLocal}" -f;
+}
